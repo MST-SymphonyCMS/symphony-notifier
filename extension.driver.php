6d4e6bac5062983d212031b9a05736022c4b25c0
tmslnz <t@theworkers.net>
2012-11-09 18:46:23 +0000

    Starting dev branch

---
+++ extension.driver.php
@@ -0,0 +1,67 @@
+<?php
+
+	Class Extension_Notifier extends Extension{
+	    
+		public function fetchNavigation() {
+			return array(
+				array(
+					'location'	=> __('System'),
+					'name'		=> __('Notifier'),
+					'link'		=> '/',
+					'limit'		=> 'developer'
+				)
+			);
+		}
+		
+		public function getSubscribedDelegates(){
+			return array(
+				array(
+					'page' => '/publish/',
+					'delegate' => 'EntryPreDelete',
+					'callback' => 'notify'
+				),
+				array(
+					'page' => '/publish/new/',
+					'delegate' => 'EntryPostCreate',
+					'callback' => 'notify'
+				),
+				array(
+					'page' => '/publish/edit/',
+					'delegate' => 'EntryPostEdit',
+					'callback' => 'notify'
+				)
+			);
+		}
+		
+		public function notify($context){
+			
+			include_once(TOOLKIT . '/class.gateway.php');
+           	$ch = new Gateway;
+            
+            $ch->init();
+            $ch->setopt('URL', 'http://rpc.pingomatic.com/');
+            $ch->setopt('POST', 1);
+            $ch->setopt('CONTENTTYPE', 'text/xml');
+            
+            $xml = new XMLElement('methodCall');
+            $xml->appendChild(new XMLElement('methodName', 'weblogUpdates.ping'));
+            
+            $params = new XMLElement('params');
+            
+            $param = new XMLElement('param');       
+            $param->appendChild(new XMLElement('value', Symphony::Configuration()->get('sitename', 'general')));
+            $params->appendChild($param);            
+
+            $param = new XMLElement('param');
+            $param->appendChild(new XMLElement('value', URL));
+            $params->appendChild($param);    
+            
+            $xml->appendChild($params);        
+			
+            $ch->setopt('POSTFIELDS', $xml->generate(true, 0));
+
+            $ch->exec(GATEWAY_FORCE_SOCKET);
+         
+		}
+		
+	}
