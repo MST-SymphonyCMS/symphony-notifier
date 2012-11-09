db799836172f0b0aabe429483b9aef5df294e0ca
tmslnz <t@theworkers.net>
2012-11-09 19:04:57 +0000

    Hunting in the dark of PHP

--- extension.driver.php
+++ extension.driver.php
@@ -1,9 +1,22 @@
 <?php
 
 	Class Extension_Notifier extends Extension{
-		
+	    
 		public function getSubscribedDelegates(){
 			return array(
+				// Register for preferences
+				array(
+					'page' => '/system/preferences/',
+					'delegate' => 'AddCustomPreferenceFieldsets',
+					'callback' => 'appendPreferences'
+				),
+				array(
+					'page' => '/system/preferences/',
+					'delegate' => 'CustomActions',
+					'callback' => 'savePreferences'
+				),
+
+				// Register for CRUD events
 				array(
 					'page' => '/publish/',
 					'delegate' => 'EntryPreDelete',
@@ -22,34 +35,71 @@
 			);
 		}
 		
+		// Controller method
 		public function notify($context){
 			include_once(TOOLKIT . '/class.gateway.php');
-			$ch = new Gateway;
-			
-			$ch->init();
-			$ch->setopt('URL', 'http://rpc.pingomatic.com/');
-			$ch->setopt('POST', 1);
-			$ch->setopt('CONTENTTYPE', 'text/xml');
-			
-			$xml = new XMLElement('methodCall');
-			$xml->appendChild(new XMLElement('methodName', 'weblogUpdates.ping'));
-			
-			$params = new XMLElement('params');
-			
-			$param = new XMLElement('param');       
-			$param->appendChild(new XMLElement('value', Symphony::Configuration()->get('sitename', 'general')));
-			$params->appendChild($param);            
+     	$ch = new Gateway;
+      
+      $ch->init();
+      $ch->setopt('URL', 'http://rpc.notifier.com/');
+      $ch->setopt('POST', 1);
+      $ch->setopt('CONTENTTYPE', 'text/xml');
+      
+      $xml = new XMLElement('methodCall');
+      $xml->appendChild(new XMLElement('methodName', 'weblogUpdates.ping'));
+      
+      $params = new XMLElement('params');
+      
+      $param = new XMLElement('param');       
+      $param->appendChild(new XMLElement('value', Symphony::Configuration()->get('sitename', 'general')));
+      $params->appendChild($param);            
 
-			$param = new XMLElement('param');
-			$param->appendChild(new XMLElement('value', URL));
-			$params->appendChild($param);    
+      $param = new XMLElement('param');
+      $param->appendChild(new XMLElement('value', URL));
+      $params->appendChild($param);    
+      
+      $xml->appendChild($params);        
+
+      $ch->setopt('POSTFIELDS', $xml->generate(true, 0));
+
+      $ch->exec(GATEWAY_FORCE_SOCKET);
+		}
+
+		
+		public function appendPreferences($context){
+			include_once(TOOLKIT . '/class.authormanager.php');
+			include_once(TOOLKIT . '/class.sectionmanager.php');
+
+			// Fieldset and layout
+			$group = new XMLElement('fieldset');
+			$group->setAttribute('class', 'settings');
+			$legend = new XMLElement('legend', __('Notifier'));
+			$group->appendChild($legend);
+
+			$div = new XMLElement('div');
+			$div->setAttribute('class', 'group triple');
+
+			// notify url of notifier event
+			$notify_url = Symphony::Configuration()->get('notify_url', 'notifier');
+			$notify_label = Widget::Label(__('Send modification event to URL'));
+			$notify_label->appendChild(Widget::Input('settings[notifier][notify_url]', $notify_url, 'text'));
+			$group->appendChild($notify_label);
+		}
+
+		public function savePreferences() {
+		
+			// Remove existing configuration settings.
+			Symphony::Configuration()->remove('notifier');
+			Administration::instance()->saveConfig();
 			
-			$xml->appendChild($params);        
-
-			$ch->setopt('POSTFIELDS', $xml->generate(true, 0));
-
-			$ch->exec(GATEWAY_FORCE_SOCKET);
-
+			// If there are Notifier settings, format them
+			if(is_array($_POST['settings']['notifier'])){
+				foreach($_POST['settings']['notifier'] as $preference => $value){
+					if(is_array($value)){
+						$_POST['settings']['notifier'][$preference] = implode(',',$value);
+					}
+				}
+			}
 		}
 		
 	}
